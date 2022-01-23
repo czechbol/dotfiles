0b43f6e764cef962b3a604b42ad9318e4a2aaaf2
czechbol <adamludes@gmail.com>
2022-01-23 15:33:51 +0100

    qtile groups update

--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -2,13 +2,14 @@ from libqtile.config import Group, Match
 
 groups = [
     Group("DEV", layout="monadtall", matches=[Match(wm_class=["Code"])]),
-    Group("CLI", layout="bsp", matches=[Match(wm_class=["kitty"])]),
+    Group("CLI", layout="bsp"),
     Group("WWW", layout="monadtall", matches=[Match(wm_class=["vivaldi-stable"])]),
     Group("SYS", layout="monadtall", matches=[Match(wm_class=["Nemo"])]),
-    Group("DOC", layout="monadtall"),
-    Group("VBOX", layout="monadtall"),
+    Group("DOC", layout="monadtall", matches=[Match(wm_class=["Zathura", "evince"])]),
     Group("CHAT", layout="monadtall", matches=[Match(wm_class=["discord"])]),
     Group("MUS", layout="monadtall", matches=[Match(wm_class=["spotify", "Spotify"])]),
-    Group("VID", layout="monadtall"),
-    Group("GFX", layout="floating"),
+    Group("VID", layout="monadtall", matches=[Match(wm_class=["vlc"])]),
+    Group(
+        "GFX", layout="monadtall", matches=[Match(wm_class=["Inkscape", "gimp-2.10"])]
+    ),
 ]
