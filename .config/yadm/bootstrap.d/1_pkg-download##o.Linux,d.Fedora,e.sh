1ba3d9e09cb45bcbae61253a9dfcede9c3a76198
czechbol <adamludes@gmail.com>
2023-03-16 20:29:05 +0100

    Update 1_pkg-download##o.Linux,d.Fedora,e.sh

--- .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
+++ .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
@@ -1,5 +1,5 @@
 echo "max_parallel_downloads=10
-fastestmirror=True" | sudo tee -a etc/dnf/dnf.conf
+fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
 
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
