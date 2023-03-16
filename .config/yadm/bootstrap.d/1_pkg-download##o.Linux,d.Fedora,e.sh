86aa2e1c0693b339930bd12d237b03133ae82bb6
czechbol <adamludes@gmail.com>
2023-03-16 16:12:53 +0100

    update

--- .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
+++ .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
@@ -5,13 +5,17 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:TheLocehiliosan:yadm/Fedora_Rawhide/home:TheLocehiliosan:yadm.repo
 sudo dnf config-manager --add-repo https://repo.vivaldi.com/stable/vivaldi-fedora.repo
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
 
 sudo dnf upgrade --refresh -y
 sudo dnf update
 
 
 dnf install --downloadonly \ 
-    python3 python3-pip ipython texlive-scheme-basic texlive-cite texlive-latexindent latexmk cmake sqlite rust cargo ansible podman \
+    python3 python3-pip ipython texlive-scheme-basic texlive-cite texlive-latexindent latexmk cmake sqlite rust cargo ansible podman gcc-go \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
     'google-roboto*' 'mozilla-fira*' fira-code-fonts \
     kitty neovim zsh ripgrep htop neofetch direnv unzip p7zip p7zip-plugins unrar tlp tlp-rdw ImageMagick dnf-utils wine winetricks \
     qdirstat blender gimp inkscape gparted vlc \
--- .config/yadm/bootstrap.d/1_pkg-install##o.Linux,d.Fedora,e.sh
+++ .config/yadm/bootstrap.d/1_pkg-install##o.Linux,d.Fedora,e.sh
@@ -1,10 +1,13 @@
 sudo dnf install \ 
-    python3 python3-pip ipython texlive-scheme-basic texlive-cite texlive-latexindent latexmk cmake sqlite rust cargo ansible podman \
+    python3 python3-pip ipython texlive-scheme-basic texlive-cite texlive-latexindent latexmk cmake sqlite rust cargo ansible podman gcc-go \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
     'google-roboto*' 'mozilla-fira*' fira-code-fonts \
     kitty neovim zsh ripgrep htop neofetch direnv unzip p7zip p7zip-plugins unrar tlp tlp-rdw ImageMagick dnf-utils wine winetricks \
     qdirstat blender gimp inkscape gparted vlc \
     gnome-extensions-app rpi-imager gnome-tweak-tool \
     codium vivaldi-stable yadm
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
 curl -sS https://starship.rs/install.sh | sh
