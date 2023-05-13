23ae2b7b4885d7807be4e24f5843971d37ed5af3
czechbol <adamludes@gmail.com>
2023-05-13 21:27:23 +0200

    update

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkdir -p /run/media/aludes/BackupHDD/backups/$(hostname)/{etc,home}
 sudo rsync -zavx /etc/ /run/media/aludes/BackupHDD/backups/$(hostname)/etc/
-rsync -zavx \
+sudo rsync -zavx \
   --exclude .cache/ --exclude .venv/ --exclude __pycache__/ --exclude .mypy_cache/ \
   --exclude ~/.gvfs/ --exclude ~/.thumbnails/ --exclude ~/local/share/containers/ \
   --exclude Trash/ --exclude ~/.Trash/ \
+  --delete \
   ~/ /run/media/aludes/BackupHDD/backups/$(hostname)/home/
