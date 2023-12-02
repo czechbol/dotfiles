26fabef03b182c0e7579a736645421d0e099502b
czechbol <adamludes@gmail.com>
2023-12-02 15:57:23 +0100

    update

--- .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
+++ .config/yadm/bootstrap.d/1_pkg-download##o.Linux,d.Fedora,e.sh
@@ -1,12 +1,11 @@
 echo "max_parallel_downloads=10
 fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
 
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://repo.vivaldi.com/stable/vivaldi-fedora.repo
-sudo dnf config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
 sudo dnf upgrade --refresh -y
 sudo dnf update
--- .config/yadm/bootstrap.d/1_pkg-install##o.Linux,d.Fedora,e.sh
+++ .config/yadm/bootstrap.d/1_pkg-install##o.Linux,d.Fedora,e.sh
@@ -1,10 +1,14 @@
 sudo dnf -y install \
-    python3 python3-pip ipython texlive-scheme-basic texlive-cite texlive-latexindent latexmk cmake sqlite rust cargo ansible podman git-delta \
+    python3 python3-pip ipython cmake sqlite rust cargo ansible podman git-delta \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
     'google-roboto*' 'mozilla-fira*' fira-code-fonts \
     kitty neovim zsh ripgrep htop neofetch direnv unzip p7zip p7zip-plugins unrar tlp tlp-rdw ImageMagick dnf-utils wine winetricks \
     qdirstat blender gimp inkscape gparted vlc \
     gnome-extensions-app rpi-imager gnome-tweak-tool \
-    codium vivaldi-stable
+    code vivaldi-stable
+
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
 
 curl -sS https://raw.githubusercontent.com/starship/starship/master/install/install.sh | sh
 
